29da888361cb429bd25a017adac090eff96db6ff
bue221 <camiloplaza3@gmail.com>
2020-09-21 06:15:17 -0500

    Updates

---
+++ precache-manifest.6f3a3bea6c366fdb675f92841e3411d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21dc551af87cab8a192f176eb845421b",
+    "url": "/Prueba/index.html"
+  },
+  {
+    "revision": "54b45c15fffa7a915e03",
+    "url": "/Prueba/static/css/main.2f3b2f09.chunk.css"
+  },
+  {
+    "revision": "5bb5ed8b3c84a75d183f",
+    "url": "/Prueba/static/js/2.dd1c01c0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Prueba/static/js/2.dd1c01c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b45c15fffa7a915e03",
+    "url": "/Prueba/static/js/main.151d7e32.chunk.js"
+  },
+  {
+    "revision": "381ee75e3774194cb9d1",
+    "url": "/Prueba/static/js/runtime-main.d9bf1a91.js"
+  }
+]);
